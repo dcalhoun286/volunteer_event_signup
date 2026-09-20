bf7e563bc22e2c7815c2693f0485b9fcaeeaddb8
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2026-09-19 22:37:42 -0700

    render ErrorBoundary wrapped around App instead

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,6 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import ErrorBoundary from '../components/error-handling/error-boundary';
 import { Events } from '../components/events/events';
 import { Home } from '../Home';
 import { Header } from '../components/layout/header';
@@ -39,41 +38,39 @@ const RoutesComponent = () => {
   }
 
   return (
-    <ErrorBoundary>
-      <Router>
-        <div className="vw-100 vh-100 primary-color d-flex justify-content-center">
-          <div className="vw-100 vh-100 container secondary-color">
-            <Routes>
+    <Router>
+      <div className="vw-100 vh-100 primary-color d-flex justify-content-center">
+        <div className="vw-100 vh-100 container secondary-color">
+          <Routes>
+            <Route
+              element={
+                <>
+                  <Header />
+                  <Main>
+                    <Outlet />
+                  </Main>
+                  <Footer />
+                </>
+              }
+            >
+              <Route path="/" element={<Home />} />
               <Route
+                path="/events"
                 element={
-                  <>
-                    <Header />
-                    <Main>
-                      <Outlet />
-                    </Main>
-                    <Footer />
-                  </>
+                  <PrivateRoute isLoggedIn={isAuthenticated}>
+                    <Events />
+                  </PrivateRoute>
                 }
-              >
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/events"
-                  element={
-                    <PrivateRoute isLoggedIn={isAuthenticated}>
-                      <Events />
-                    </PrivateRoute>
-                  }
-                />
-                {import.meta.env.DEV && (
-                  <Route path="/test-error" element={<TestErrorPage />} />
-                )}
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-          </div>
+              />
+              {import.meta.env.DEV && (
+                <Route path="/test-error" element={<TestErrorPage />} />
+              )}
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
         </div>
-      </Router>
-    </ErrorBoundary>
+      </div>
+    </Router>
   );
 };
 
